e3729bf61f58dbd592fd62647018a76895f3fe6a
sodubr0mek <sodubromek@gmail.com>
2026-06-08 20:59:43 +0200

    Sync between main and test

--- main.c
+++ main.c
@@ -1,11 +1,108 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
-//subfile used for testing//
+//main file
 
 int funMenu(void)
 {
-    
+    int c;
+    int k;
+    int funcn = 2;
+    int loopin = 1;
+    char funcarray[3];
+    funcarray[0] = '>';
+    funcarray[1] = ' ';
+    funcarray[2] = ' ';
+    clear();
+    printw("\n  FUNCTIONS MENU\n%c 1 : cut recent word\n%c 2 : cut recent line\n%c 3 : save as file\n\n", funcarray[0], funcarray[1], funcarray[2]);
+    refresh();
+
+    int pick = 0;
+
+    while (loopin == 1)
+    {
+        c = getch();
+
+
+        if (c == 115)
+        {
+            pick++;
+        }
+        if (c == 119)
+        {
+            pick--;
+        }
+
+
+        //enter RETURNS - 1z, 2k
+        if (c == 10)
+        {
+            if (pick == 0)
+            {
+                clear();
+                refresh();
+                return 1;
+            }
+            if (pick == 1)
+            {
+                clear();
+                refresh();
+                return 2;
+            }
+            if (pick == 2)
+            {
+                clear();
+                refresh();
+                return 3;
+            }
+        }
+        if (c == 49)
+        {
+            clear();
+            refresh();
+            return 1;
+        }
+        if (c == 50)
+        {
+            clear();
+            refresh();
+            return 2;
+        }
+        if (c == 51)
+        {
+            clear();
+            refresh();
+            return 3;
+        }
+
+
+        if (pick == -1)
+        {
+            pick = funcn;
+        }
+        if (pick == funcn + 1)
+        {
+            pick = 0;
+        }
+
+        for(k = 0; k<= funcn; k++)
+        {
+            funcarray[k] = ' ';
+        }
+        funcarray[pick] = '>';
+        clear();
+
+        printw("\n  FUNCTIONS MENU\n%c 1 : cut recent word\n%c 2 : cut recent line\n%c 3 : save as file\n\n", funcarray[0], funcarray[1], funcarray[2]);
+        refresh();
+    }
+
+
+
+
+
+
+
+
 }
 
 int main(void)
@@ -23,8 +120,9 @@ int main(void)
     temp = (char*)malloc(8*sizeof(char));
 
 
+
+
     initscr();
-    
     while (doLoop == 1)
     {
 
@@ -98,10 +196,9 @@ int main(void)
         //funcions//
         if(c == 1)
         {
-            printw("\n  FUNCTIONS: \n       z : cut recent word\n       k : cut line\n");
+            int calledfunc = funMenu();
             refresh();
-            c = getch();
-            if(c == 122)
+            if(calledfunc == 1)
             {
                 k = i;
                 block = 0;
@@ -128,7 +225,7 @@ int main(void)
                     k--;
                 }
             }
-            else if(c == 107)
+            else if(calledfunc == 2)
             {
                 k = i;
                 block = 0;
@@ -153,11 +250,83 @@ int main(void)
                         block = 1;
                     }
                     k--;
-                }
+                } 
             }
+            else if (calledfunc == 3)
+            {
+                //save to file
+                FILE *fptr;
+                clear();
+                printw("\n  Name file : ");
+                
+                char savf[128];
+                int enterpressed = 0;
+                int sfn;
+                int is = 1;
+                int ks = 1;
+                while (enterpressed != 1)
+                {
+                    ks = 1;
+                    sfn = getch();
+                    clear();
+                    printw("\n  Name file : ");
+                    
+
+                    if(sfn != 127 && sfn != 10)
+                    {
+                        savf[is] = (char)sfn;
+                    } 
+                    if(sfn == 10)
+                    {
+                        enterpressed = 1;
+
+                    }  
+                    if(sfn == 127 && is > 2)
+                    {
+                        memset(&savf[is-1], 0, sizeof(char));
+                        is = is-2;
+                    }
+                    else if(sfn == 127 && is == 2)
+                    {
+                        memset(&savf[1], 0 , sizeof(char));
+                        is = 0;
+                    }
+                    else if(sfn == 127 && is == 1)
+                    {
+                        is = 0;
+                    }
+
+
+                    if(strlen(savf) == 0 && is == 2)
+                    {
+                        is = -1;
+                    }
+
+                    for(ks = 1; ks<=is; ks++)
+                    {
+                        printw("%c", savf[ks]);
+                    }  
+
+                    refresh();
+                    is++;               
+                }
+                savf[is+2] = 't';
+                savf[is+1] = 'x';
+                savf[is] = 't';
+                savf[is-1] = '.';
+                memset(&savf[is+3], 0, sizeof(savf[is+3]));
+                fptr = fopen(&savf[1], "w");
+                fptr = fopen(&savf[1], "w");
+                fprintf(fptr, &znaki[1]);
+
+                fclose(fptr); 
+
+
+            }
+        }
+            
             
             clear();
-        }
 
         for (k = 1; k <= i; k++)
         {
